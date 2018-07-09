2d8143d6c64fce32c69d6b8194033674c8bbfa0a
niknorp <40911513+niknorp@users.noreply.github.com>
2018-07-09 18:16:32 +0300

    initial proj commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  notateObjects = [
+    {
+      title: 'Hello',
+      description: 'Lorem ipsum dolor',
+      price: 45.99
+    },
+    {
+      title: 'Hello',
+      description: 'Lorem ipsum dolor',
+      price: 45.99
+    },
+    {
+      title: 'Hello',
+      description: 'Lorem ipsum dolor',
+      price: 45.99
+    },
+    {
+      title: 'Hello',
+      description: 'Lorem ipsum dolor',
+      price: 45.99
+    },
+    {
+      title: 'Hello',
+      description: 'Lorem ipsum dolor',
+      price: 45.99
+    },
+    {
+      title: 'Hello',
+      description: 'Lorem ipsum dolor',
+      price: 45.99
+    }
+  ];
+}
